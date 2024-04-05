a9e2b972da0f47081a83600cf0e88e897fe0c238
Максим Макаров <maksoncheus@gmail.com>
2024-04-05 13:27:30 +0700

    Попытка сделать оценки

---
+++ E-Diary.WEB/Data/Entities/Grade.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Diary.WEB.Data.Entities
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public virtual Lesson Lesson { get; set; }
+        //here need to find corresponding student
+        public virtual User User { get; set; }
+        public bool IsMissed { get; set; }
+    }
+}
